2ce20f7162671b4a739350934b84e7c6ee5f96ac
Kayee Zhou <kayee@utexas.edu>
2025-06-24 14:13:22 -0500

    Handle missing driver camera and registration

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -65,6 +65,8 @@ ENABLED_STATES = (State.preEnabled, *ACTIVE_STATES)
 
 
 class Controls:
+  d_camera_hardware_missing = True
+
   def __init__(self, CI=None):
     self.params = Params()
 
@@ -92,12 +94,18 @@ class Controls:
     self.sensor_packets = ["accelerometer", "gyroscope"]
     self.camera_packets = ["roadCameraState", "driverCameraState", "wideRoadCameraState"]
 
+    if self.d_camera_hardware_missing:
+      IGNORE_PROCESSES.update({"dmonitoringd", "dmonitoringmodeld"})
+      self.camera_packets.remove("driverCameraState")
+
     self.log_sock = messaging.sub_sock('androidLog')
 
     # TODO: de-couple controlsd with card/conflate on carState without introducing controls mismatches
     self.car_state_sock = messaging.sub_sock('carState', timeout=20)
 
     ignore = self.sensor_packets + ['testJoystick']
+    if self.d_camera_hardware_missing:
+      ignore += ['driverMonitoringState']
     if SIMULATION:
       ignore += ['driverCameraState', 'managerState']
     if REPLAY:
@@ -243,7 +251,8 @@ class Controls:
       self.events.add(EventName.resumeBlocked)
 
     if not self.CP.notCar:
-      self.events.add_from_msg(self.sm['driverMonitoringState'].events)
+      if not self.d_camera_hardware_missing:
+        self.events.add_from_msg(self.sm['driverMonitoringState'].events)
 
     # Add car events, ignore if CAN isn't valid
     if CS.canValid:
